548a848d2f47a3992e9d24d91e167ad48ef8fa2a
Thibault Denizet <thibault@graybike.com>
2022-07-21 09:17:58 +0700

    Test Rubocop reporting

--- app/controllers/common_incident_types_controller.rb
+++ app/controllers/common_incident_types_controller.rb
@@ -2,9 +2,7 @@ class CommonIncidentTypesController < ApplicationController
   def search
     term = "%#{params[:q]&.downcase}%"
     @results = CommonIncidentType.where("lower(code) LIKE ?", term).or(
-      CommonIncidentType.where("lower(notes) LIKE ?", term).or(
-        CommonIncidentType.where("lower(description) LIKE ?", term),
-      ),
+      CommonIncidentType.where("lower(notes) LIKE ?", term).or(CommonIncidentType.where("lower(description) LIKE ?", term),),
     )
     render layout: false
   end
