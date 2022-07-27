cd8029681d0b0641a0fe95c2544a20af7ae3e712
Thibault Denizet <thibault@graybike.com>
2022-07-27 15:19:24 +0700

    Add my profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  layout :layout_by_resource
+
+  # Use logged-in layout when editing current_user details
+  def layout_by_resource
+    if devise_controller? && defined?(resource_name) && !my_profile?
+      "devise"
+    else
+      "application"
+    end
+  end
+
+  def my_profile?
+    (
+      [resource_name, action_name] == [:user, "edit"] ||
+      [resource_name, action_name] == [:user, "update"]
+    )
+  end
 end
