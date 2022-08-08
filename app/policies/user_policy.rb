a6f1f418bea63e7bb4852c1685aab66a1a439294
Thibault Denizet <thibault@graybike.com>
2022-08-08 11:36:06 +0700

    Add comment about user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,3 +1,15 @@
+# This policy focused on preventing users
+# from accessing, updating or destroying their user record.
+# It might seem confusing since users can actually update their own data.
+# But this policy is actually only used in the UsersController
+# which provides a way for administrators to check, update or
+# destroy users. This was implemented this way to prevent
+# an administrator from accidentaly changing their role and
+# losing permissions.
+#
+# In order to update their own records, administrators
+# will need to use the "My Profile" page instead.
+#
 class UserPolicy < ApplicationPolicy
   def show
     @current_user != @record
