df79e5cb066c49d188e7d68641072af752879a5b
Thibault Denizet <thibault@graybike.com>
2022-08-08 11:37:44 +0700

    Fix typo in user policy comment

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,10 +1,10 @@
 # This policy focused on preventing users
-# from accessing, updating or destroying their user record.
+# from accessing, updating, or destroying their user records.
 # It might seem confusing since users can actually update their own data.
 # But this policy is actually only used in the UsersController
 # which provides a way for administrators to check, update or
 # destroy users. This was implemented this way to prevent
-# an administrator from accidentaly changing their role and
+# an administrator from accidentally changing their role and
 # losing permissions.
 #
 # In order to update their own records, administrators
