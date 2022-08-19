ed169ed4ae00360fdbcdc0cf0786e0b06e9f97b7
Thibault Denizet <thibault@graybike.com>
2022-08-19 15:27:11 +0700

    Add Capybara-screenshots

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,11 +5,15 @@ require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require "rspec/rails"
+require "capybara/rspec"
+require "capybara-screenshot/rspec"
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 Dir["./spec/support/**/*.rb"].each { |f| require f }
 
-Capybara.javascript_driver = :selenium_headless
+Capybara.javascript_driver = :selenium
+Capybara::Screenshot.prune_strategy = { keep: 20 }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
