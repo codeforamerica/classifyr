a6c0751f19a788a5cb991ff379cded6da9765c47
Thibault Denizet <thibault@graybike.com>
2022-08-26 12:27:16 +0700

    Make dismissible alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,5 +72,16 @@ module ApplicationHelper
 
     ""
   end
+
+  def tailwind_classes_for(flash_type)
+    {
+      notice: "bg-green-100 border-l-4 border-green-700 text-white",
+      error: "bg-red-300 border-l-4 border-red-700 text-black",
+    }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  def alert_background_for(flash_type)
+    flash_type == :notice ? "text-green-800" : "text-red-900"
+  end
 end
 # rubocop:enable Metrics/MethodLength
