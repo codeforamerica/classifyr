de76cd56663aea4cb54300d727247b04a0334b5c
Thibault Denizet <thibault@graybike.com>
2022-09-01 17:00:42 +0700

    Move styling for auth notice to stylesheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -75,8 +75,8 @@ module ApplicationHelper
 
   def tailwind_classes_for(flash_type)
     {
-      notice: "bg-green-100 border-l-4 border-green-700 text-white",
-      error: "bg-red-300 border-l-4 border-red-700 text-black",
+      notice: "authenticated-notice",
+      error: "authenticated-error",
     }.stringify_keys[flash_type.to_s] || flash_type.to_s
   end
 
